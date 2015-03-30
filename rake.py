968e2b18f763f2efaed91799d9b1bff6c738c009
SiddarthaNagireddy <siddartha.nagireddy@gmail.com>
2015-03-30 22:51:03 +0000

    Version 1.1
    Update on Rake

--- rake.py
+++ rake.py
@@ -1,10 +1,15 @@
+'''
+Created on Mar 27, 2015
+
+@author: Siddartha.Reddy
+'''
 import re
 import operator
 import csv
-import mmap
 
 debug = False
 test = True
+stoppath = "SmartStoplist.txt"
 
 def is_number(s):
     try:
@@ -13,6 +18,7 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def load_stop_words(stop_word_file):
     stop_words = []
     for line in open(stop_word_file):
@@ -20,6 +26,7 @@ def load_stop_words(stop_word_file):
             for word in line.split():  # in case more than one per line
                 stop_words.append(word)
     return stop_words
+
 
 def separate_words(text, min_word_return_size):
     splitter = re.compile('[^a-zA-Z0-9_\\+\\-/]')
@@ -31,10 +38,12 @@ def separate_words(text, min_word_return_size):
             words.append(current_word)
     return words
 
+
 def split_sentences(text):
     sentence_delimiters = re.compile(u'[.!?,;:\t\\-\\"\\(\\)\\\'\u2019\u2013]')
     sentences = sentence_delimiters.split(text)
     return sentences
+
 
 def build_stop_word_regex(stop_word_file_path):
     stop_word_list = load_stop_words(stop_word_file_path)
@@ -44,6 +53,7 @@ def build_stop_word_regex(stop_word_file_path):
         stop_word_regex_list.append(word_regex)
     stop_word_pattern = re.compile('|'.join(stop_word_regex_list), re.IGNORECASE)
     return stop_word_pattern
+
 
 def generate_candidate_keywords(sentence_list, stopword_pattern):
     phrase_list = []
@@ -55,6 +65,7 @@ def generate_candidate_keywords(sentence_list, stopword_pattern):
             if phrase != "":
                 phrase_list.append(phrase)
     return phrase_list
+
 
 def calculate_word_scores(phraseList):
     word_frequency = {}
@@ -81,6 +92,7 @@ def calculate_word_scores(phraseList):
     #word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
     return word_score
 
+
 def generate_candidate_keyword_scores(phrase_list, word_score):
     keyword_candidates = {}
     for phrase in phrase_list:
@@ -92,9 +104,10 @@ def generate_candidate_keyword_scores(phrase_list, word_score):
         keyword_candidates[phrase] = candidate_score
     return keyword_candidates
 
+
 class Rake(object):
-    def __init__(self, stop_words_path):
-        self.stop_words_path = stop_words_path
+    def __init__(self,stoppath):
+        self.stop_words_path = stoppath
         self.__stop_words_pattern = build_stop_word_regex(stoppath)
 
     def run(self, text):
@@ -105,27 +118,7 @@ class Rake(object):
         sorted_keywords = sorted(keyword_candidates.iteritems(), key=operator.itemgetter(1), reverse=True)
         return sorted_keywords
 
-if test:
-    ''' Fetch the stop words '''
-    stoppath = "smartstoplist.txt"
-    rake = Rake("smartstoplist.txt")
-    string = ''
-    fd = open('C:/Users/Siddartha.Reddy/Desktop/testfiles/contacts_segments_title000','r')
-    wr = open('C:/Users/Siddartha.Reddy/Desktop/testfiles/processed_title','w')
-    data = csv.reader(fd,delimiter='\t',quoting=csv.QUOTE_NONE)
-    countLine = 0
-    for line in data:
-        countLine += 1
-        text = line[2]
-        if '/' in text:
-            text = text.replace('/',' and ')
-        elif '&' in text:
-            text = text.replace('&',' and ')
-        keywords = rake.run(text)
-        for x in range(0,len(keywords)):
-            wr.write(str(line) + '\t' + str(keywords[x][0]) + '\n')
-        if countLine == 100000:
-            print('Done writing:',countLine)
-            countLine = 0
-    wr.close()
-    fd.close()
+def execute_rake(text):
+    rake = Rake('smartstoplist.txt')
+    keywords = rake.run(text)
+    return keywords
